2988df27fd218dcd54112103c543e0bd3b98ad39
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-08-23 23:02:43 +0300

    deserve > dev: server/api

--- packages/deserve/source/server/api/modules/general/store/schemas/index.ts
+++ packages/deserve/source/server/api/modules/general/store/schemas/index.ts
@@ -16,18 +16,49 @@ export const queries = gql`
 
 export const mutations = gql`
     extend type Mutation {
-
+        requestBlob(input: InputRequestBlob!): ResponseBlob!
+        requestData(input: InputRequestData!): ResponseData!
     }
 `;
 
 
 export const types = gql`
+    type ResponseBlob {
+        status: Boolean!
+        error: Error
+        data: Blob
+    }
 
+    type Blob {
+        # The source is a temporary URL where the blob lives
+        # and is downloadable (public, or with authentication).
+        source: String!
+    }
+
+    type ResponseData {
+        status: Boolean!
+        error: Error
+        data: Data
+    }
+
+    type Data {
+        # The value can be an actual string, or any other kind of value
+        # (boolean, number, object, array). The consumer mut ensure
+        # that the correct type is consumed.
+        value: String!
+    }
 `;
 
 
 export const inputs = gql`
+    input InputRequestBlob {
+        id: String!
+    }
 
+    input InputRequestData {
+        entity: String!
+        id: String!
+    }
 `;
 // #endregion module
 
