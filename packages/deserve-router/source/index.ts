203567656c6fbbb067a362ef5d24140f80396fa8
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-09-13 21:29:28 +0300

    deserve-router > dev: index

--- packages/deserve-router/source/index.ts
+++ packages/deserve-router/source/index.ts
@@ -13,8 +13,6 @@
 const server = express();
 const port = process.env.PORT || 3344;
 
-const tunnels: any = {};
-
 const handlePaths = (
     request: Request,
     response: Response,
@@ -22,8 +20,27 @@ const handlePaths = (
     response.send('Deserve Router');
 }
 
+const handleRegister = (
+    request: Request,
+    response: Response,
+) => {
+    const responseData = {
+        status: true,
+        data: {
+            core: 'https://a-core.data.domain.tld',
+        },
+    };
+    response.setHeader(
+        'Content-Type',
+        'application/json',
+    );
+    response.send(JSON.stringify(responseData));
+}
+
 const main = () => {
-    server.get('*', handlePaths);
+    server.get('/', handlePaths);
+
+    server.post('/register', handleRegister);
 
     server.listen(port, () => {
         console.log(`\n\tDeserve Router Server on /, port ${port}\n\thttp://localhost:${port}`);
