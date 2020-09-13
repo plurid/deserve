69d47211a91078355dc8b20d068eadb2faa14553
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-09-13 21:30:12 +0300

    deserve-node > dev: server/logic/registration

---
+++ packages/deserve-node/source/server/logic/registration/index.ts
@@ -0,0 +1,23 @@
+// #region imports
+    // #region libraries
+
+    // #endregion libraries
+// #endregion imports
+
+
+
+// #region module
+const registerNode = async (
+    url: string,
+) => {
+    // const coreResponse = await
+}
+// #endregion module
+
+
+
+// #region exports
+export {
+    registerNode,
+};
+// #endregion exports
