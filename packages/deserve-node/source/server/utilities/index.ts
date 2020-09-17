8ebd4e6a4569953fa5d528d9caa475e2bc284ba8
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-09-17 16:47:56 +0300

    deserve-node > dev: server/utilities/key

--- packages/deserve-node/source/server/utilities/index.ts
+++ packages/deserve-node/source/server/utilities/index.ts
@@ -1,6 +1,7 @@
 // #region exports
 export * from './directory';
 export * from './error';
+export * from './key';
 export * from './logging';
 export * from './owner';
 // #endregion exports
---
+++ packages/deserve-node/source/server/utilities/key/index.ts
@@ -0,0 +1,41 @@
+// #region imports
+    // #region libraries
+    import bcrypt from 'bcrypt';
+    // #endregion libraries
+// #endregion imports
+
+
+
+// #region module
+/**
+ * Returns a hashed string for the `keyText`.
+ *
+ * @param keyText
+ */
+export const hashKey = async (
+    keyText: string,
+): Promise<string> => {
+    try {
+        const saltRounds = 12;
+        const hashedKey = await bcrypt.hash(keyText, saltRounds);
+
+        return hashedKey;
+    } catch (error) {
+        throw 'Could not Hash Key.';
+    }
+}
+
+
+/**
+ * Validates that the `keyText` is equal to the `hash`.
+ *
+ * @param keyText
+ * @param hash
+ */
+export const validateKey = async (
+    keyText: string,
+    hash: string,
+) => {
+    return await bcrypt.compare(keyText, hash);
+}
+// #endregion module
