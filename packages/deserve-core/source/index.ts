c32f06433894646b2f3319a8389824b0154191c9
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-09-21 21:19:19 +0300

    deserve-core > setup: package

--- packages/deserve-core/source/index.ts
+++ packages/deserve-core/source/index.ts
@@ -10,6 +10,10 @@
     import cors from 'cors';
     import bodyParser from 'body-parser';
     import cookieParser from 'cookie-parser';
+
+
+    import Koa from 'koa';
+    import Router from 'koa-router';
     // #endregion libraries
 
     // #region internal
@@ -22,9 +26,11 @@
 
 // #region module
 const server = express();
-const port = process.env.PORT || 3655;
+const port = parseInt(process.env.PORT || '') || 3355;
+
 
 let client: any;
+
 
 const handlePaths = (
     request: Request,
@@ -32,6 +38,7 @@ const handlePaths = (
 ) => {
     response.send('Deserve Core');
 }
+
 
 const registerTunnel = async (
     request: Request,
@@ -59,7 +66,6 @@ const registerTunnel = async (
 
     // establish connection
     const id = Math.random() + '';
-    // id = 'onetwothree';
 
     const agent = new TunnelAgent({
         clientId: id,
@@ -102,11 +108,96 @@ const corsOptions = {
 }
 
 
-const main = () => {
-    // server.options('*', cors(corsOptions));
+const mainKoa = () => {
+    const app = new Koa();
+    const router = new Router();
+
+
+    router.post(
+        '/register',
+        async (ctx, next) => {
+            if (client) {
+                await next();
+                return;
+            }
+
+            const path = ctx.request.path;
+            console.log('post register ctx', path);
+
+            const id = Math.random() + '';
+
+            const agent = new TunnelAgent({
+                clientId: id,
+                maxSockets: 10,
+            });
+
+            try {
+                const info: any = await agent.listen();
+
+                client = new Client({
+                    id,
+                    agent,
+                });
+
+                client.on('offline', () => {
+                    client = null;
+                });
+
+                const responseData = {
+                    id: id,
+                    port: info.port,
+                    max_conn_count: 10,
+                };
+                ctx.body = responseData;
+            } catch (error) {
+                const responseData = {};
+                ctx.body = responseData;
+            }
+        },
+    );
+
+    app.use(router.routes());
+    app.use(router.allowedMethods());
+
+
+    const server = http.createServer();
+
+    const appCallback = app.callback();
+
+    server.on('request', (req, res) => {
+        console.log('client', client);
+
+        const path = req.url;
+        console.log('server request', path);
+
+        if (!client) {
+            appCallback(req, res);
+            return;
+        }
+
+        client.handleRequest(req, res);
+    });
+
+    server.on('upgrade', (req, socket, head) => {
+        if (!client) {
+            socket.destroy();
+            return;
+        }
+
+        client.handleUpgrade(req, socket);
+    });
+
+    server.listen(3355, 'localhost', () => {
+        console.log(`\n\tDeserve Core Server on /, port ${port}\n\thttp://localhost:${port}`);
+    });
+}
+
+
+const mainExpress = () => {
+    server.options('*', cors(corsOptions));
 
     server.use(
-        // cors(corsOptions),
+        cors(corsOptions),
         bodyParser.json(),
         cookieParser(),
     );
@@ -168,5 +259,7 @@ const main = () => {
     });
 }
 
-main();
+
+mainKoa();
+// mainExpress();
 // #endregion module
