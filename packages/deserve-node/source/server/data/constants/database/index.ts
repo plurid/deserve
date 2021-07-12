115aa13be40a6a1a5a08ae9debf14925085ccb9d
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-07-12 10:03:51 +0300

    deserve-node > dev: server/data | database

--- packages/deserve-node/source/server/data/constants/database/index.ts
+++ packages/deserve-node/source/server/data/constants/database/index.ts
@@ -1,4 +1,5 @@
 // #region module
 export const DESERVE_DATABASE = 'deserve';
 export const DESERVE_GLOBAL = 'global';
+export const DESERVE_DATA = 'data';
 // #endregion module
--- packages/deserve-node/source/server/logic/database/collections/index.ts
+++ packages/deserve-node/source/server/logic/database/collections/index.ts
@@ -8,6 +8,7 @@
     import {
         DESERVE_DATABASE,
         DESERVE_GLOBAL,
+        DESERVE_DATA,
     } from '~server/data/constants';
 
     import {
@@ -22,4 +23,6 @@
 export const getDeserveDatabase = async () => (await database)?.db(DESERVE_DATABASE);
 
 export const getDeserveGlobalCollection = async <T = any>() => (await getDeserveDatabase())?.collection<T>(DESERVE_GLOBAL);
+
+export const getDeserveDataCollection = async <T = any>() => (await getDeserveDatabase())?.collection<T>(DESERVE_DATA);
 // #endregion module
