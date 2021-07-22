aee121a8e11cb1d2d13b87a05ef57939f4903862
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-07-22 19:38:12 +0300

    deserve-node > setup: scripts

---
+++ packages/deserve-node/scripts/setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+
+
+echo "Running MongoDB Setup"
+
+docker pull mongo
+
+sudo mkdir -p /deserve_node_database
+
+DESERVE_NODE_MONGO_USERNAME=`openssl rand -base64 32`
+DESERVE_NODE_MONGO_PASSWORD=`openssl rand -base64 32`
+
+docker run -it \
+    -v deserve_node_database:/data/db \
+    --name deserve_node_mongodb \
+    -p 33734:27017 \
+    -e MONGO_INITDB_ROOT_USERNAME=$DESERVE_NODE_MONGO_USERNAME \
+    -e MONGO_INITDB_ROOT_PASSWORD=$DESERVE_NODE_MONGO_PASSWORD \
+    -d \
+    mongo
+
+echo "Safely store the\nMONGO_INITDB_ROOT_USERNAME $DESERVE_NODE_MONGO_USERNAME\nand the\nMONGO_INITDB_ROOT_PASSWORD $DESERVE_NODE_MONGO_PASSWORD"
+
+echo "--- MongoDB Setup Finished ---"
+
+
+
+
+echo "Running MinIO Setup"
+
+docker pull minio/minio
+
+sudo mkdir -p /deserve_node_storage
+
+DESERVE_NODE_MINIO_ACCESS_KEY=`openssl rand -base64 32`
+DESERVE_NODE_MINIO_SECRET_KEY=`openssl rand -base64 32`
+
+docker run -it \
+    -v deserve_node_storage:/data \
+    --name deserve_node_minio \
+    -p 33735:9000 \
+    -e MINIO_ROOT_USER=$DESERVE_NODE_MINIO_ACCESS_KEY \
+    -e MINIO_ROOT_PASSWORD=$DESERVE_NODE_MINIO_SECRET_KEY \
+    -d \
+    minio/minio
+
+echo "Safely store the\nMINIO_ROOT_USER $DESERVE_NODE_MINIO_ACCESS_KEY\nand the\nMINIO_ROOT_PASSWORD $DESERVE_NODE_MINIO_SECRET_KEY"
+
+echo "--- MinIO Setup Finished ---"
+
+
+
+
+echo "Running Deserve Node Setup"
+
+docker pull deserve-node
+
+docker run \
+    -p 33733:33733 \
+    -d \
+    deserve-node
+
+echo "--- Deserve Node Setup Finished ---"
