53b4653a535c97977f64f46ef3a38c796e26cfbc
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-05 22:26:33 +0300

    deserve-node > dev: server/api/models/store/mutation/storeFunction

--- packages/deserve-node/source/server/api/models/store/mutation/storeFunction/index.ts
+++ packages/deserve-node/source/server/api/models/store/mutation/storeFunction/index.ts
@@ -1,6 +1,11 @@
 // #region imports
     // #region libraries
     import delog from '@plurid/delog';
+
+    import {
+        uuid,
+        sha,
+    } from '@plurid/plurid-functions';
     // #endregion libraries
 
 
@@ -10,6 +15,12 @@
 
         InputStoreFunction,
     } from '~server/data/interfaces';
+
+    import database from '~server/services/database';
+
+    import {
+        getCoreFromRequest,
+    } from '~server/logic/core';
     // #endregion external
 // #endregion imports
 
@@ -22,12 +33,53 @@ const storeFunction = async (
 ): Promise<any> => {
     try {
         const {
+            request,
+            owner,
+            collections,
+        } = context;
+
+        const {
             name: functionName,
             text: functionText,
             database: functionDatabase,
             storage: functionStorage,
             externals: functionExternals,
         } = input;
+
+        const core = await getCoreFromRequest(request);
+
+        const ownerID = owner?.id || core.ownerID;
+        if (!ownerID) {
+            return {
+                status: false,
+            };
+        }
+
+
+        const functionID = ownerID + '/' + uuid.generate() + uuid.generate() + uuid.generate();
+        const storedAt = Date.now();
+        const functionSHA = await sha.compute(ownerID + storedAt + functionText);
+
+        const stored = await database.updateDocument(
+            collections.functions,
+            functionID,
+            {
+                name: functionName,
+                text: functionText,
+                database: functionDatabase,
+                storage: functionStorage,
+                externals: functionExternals,
+                sha: functionSHA,
+            },
+        );
+
+
+        if (!stored) {
+            return {
+                status: false,
+            };
+        }
+
 
         return {
             status: true,
