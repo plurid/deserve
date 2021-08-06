c6a9e046f88a9e671e2c0c6b9a8c62c937cce148
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-06 21:15:11 +0300

    deserve-node > dev: server/api/modules/general/functioner

--- packages/deserve-node/source/server/api/modules/general/functioner/resolvers/mutation/index.ts
+++ packages/deserve-node/source/server/api/modules/general/functioner/resolvers/mutation/index.ts
@@ -15,6 +15,31 @@
 
 // #region exports
 export default {
+    functionerDatabaseSet: (
+        _: any,
+        { input }: InputOf<any>,
+        context: Context,
+    ) => Functioner.Mutation.databaseSet(
+        input,
+        context,
+    ),
+    functionerDatabaseRemove: (
+        _: any,
+        { input }: InputOf<any>,
+        context: Context,
+    ) => Functioner.Mutation.databaseRemove(
+        input,
+        context,
+    ),
 
+
+    functionerStorageRemove: (
+        _: any,
+        { input }: InputOf<any>,
+        context: Context,
+    ) => Functioner.Mutation.storageRemove(
+        input,
+        context,
+    ),
 };
 // #endregion exports
--- packages/deserve-node/source/server/api/modules/general/functioner/resolvers/query/index.ts
+++ packages/deserve-node/source/server/api/modules/general/functioner/resolvers/query/index.ts
@@ -15,6 +15,47 @@
 
 // #region exports
 export default {
+    functionerDatabaseGet: (
+        _: any,
+        { input }: InputOf<any>,
+        context: Context,
+    ) => Functioner.Query.databaseGet(
+        input,
+        context,
+    ),
+    functionerDatabaseQuery: (
+        _: any,
+        { input }: InputOf<any>,
+        context: Context,
+    ) => Functioner.Query.databaseQuery(
+        input,
+        context,
+    ),
+    functionerDatabaseGetFunctionArguments: (
+        _: any,
+        { input }: InputOf<any>,
+        context: Context,
+    ) => Functioner.Query.databaseGetFunctionArguments(
+        input,
+        context,
+    ),
+    functionerDatabaseGetFunctionData: (
+        _: any,
+        { input }: InputOf<any>,
+        context: Context,
+    ) => Functioner.Query.databaseGetFunctionData(
+        input,
+        context,
+    ),
 
+
+    functionerEventEmit: (
+        _: any,
+        { input }: InputOf<any>,
+        context: Context,
+    ) => Functioner.Query.eventEmit(
+        input,
+        context,
+    ),
 };
 // #endregion exports
