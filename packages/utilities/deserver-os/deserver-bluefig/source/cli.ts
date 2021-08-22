a582e0400976ea4d69c8727ab937d9c1c6c15e36
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-22 21:56:34 +0300

    utilities/deserver-os/deserver-bluefig > dev: cli

--- packages/utilities/deserver-os/deserver-bluefig/source/cli.ts
+++ packages/utilities/deserver-os/deserver-bluefig/source/cli.ts
@@ -1,4 +1,56 @@
+// #region imports
+    // #region libraries
+    import os from 'os';
+    import path from 'path';
+    import fsSync, {
+        promises as fs,
+    } from 'fs';
+    // #endregion libraries
+// #endregion imports
+
+
+
 // #region module
+const deploy = async () => {
+    console.log('\n\tDeploying deserver bluefig configuration.\n');
+
+    const files = [
+        'hooks.js',
+        'views.js',
+    ];
+
+    const bluefigDirectory = path.join(
+        os.homedir(),
+        './.bluefig',
+    );
+    const bluefigExists = fsSync.existsSync(
+        bluefigDirectory,
+    );
+    if (!bluefigExists) {
+        fs.mkdir(bluefigDirectory);
+    }
+
+    for (const file of files) {
+        await fs.copyFile(
+            path.join(
+                __dirname,
+                '../distribution/' + file,
+            ),
+            path.join(
+                os.homedir(),
+                './.bluefig/' + file,
+            ),
+        );
+    }
+}
+
+
+const update = async () => {
+    console.log('\n\tUpdating deserver bluefig configuration.\n');
+
+}
+
+
 const cli = () => {
     const arg = process.argv[2];
     if (!arg) {
@@ -10,10 +62,10 @@ const cli = () => {
 
     switch (command) {
         case 'deploy':
-            console.log('\n\tDeploying deserver bluefig configuration.\n');
+            deploy();
             break;
         case 'update':
-            console.log('\n\tUpdating deserver bluefig configuration.\n');
+            update();
             break;
         default:
             console.log(`\n\tCommand '${command}' is unknown. Allowed commands: deploy, update.\n`);
