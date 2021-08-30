2a4c305bec71e4e299a5e296d66c18374c68ce18
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-30 14:26:20 +0300

    utilities/deserver-os/deserver-bluefig > dev: utilities/deon

--- packages/utilities/deserver-os/deserver-bluefig/source/utilities/deon/index.ts
+++ packages/utilities/deserver-os/deserver-bluefig/source/utilities/deon/index.ts
@@ -1,6 +1,7 @@
 // #region imports
     // #region libraries
-    import {
+    import path from 'path';
+    import fsSync, {
         promises as fs,
     } from 'fs';
 
@@ -12,31 +13,49 @@
 
 // #region module
 export const readDeonFile = async <T = any>(
-    path: string,
+    filepath: string,
 ) => {
     try {
         const deon = new Deon();
-        const data = await deon.parseFile<T>(path);
+        const data = await deon.parseFile<T>(
+            filepath,
+        );
 
         return data;
     } catch (error) {
+        console.log('readDeonFile error', error);
+
         return;
     }
 }
 
 
 export const writeDeonFile = async <T= any>(
-    path: string,
+    filepath: string,
     data: T,
 ) => {
     try {
+        const directory = path.dirname(filepath);
+        if (!fsSync.existsSync(directory)) {
+            fsSync.mkdirSync(
+                directory,
+                {
+                    recursive: true,
+                },
+            );
+        }
+
         const deon = new Deon();
         const deonString = deon.stringify(data);
 
-        await fs.writeFile(path, deonString);
+        await fs.writeFile(
+            filepath,
+            deonString,
+        );
 
         return true;
     } catch (error) {
+        console.log('writeDeonFile error', error);
         return false;
     }
 }
