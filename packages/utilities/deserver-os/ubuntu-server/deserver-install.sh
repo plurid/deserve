7ed46a2ce55a3d58d2311a0df2a81fbee69eea93
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-31 19:39:58 +0300

    utilities/deserver-os/ubuntu-server > dev: install

--- packages/utilities/deserver-os/ubuntu-server/deserver-install.sh
+++ packages/utilities/deserver-os/ubuntu-server/deserver-install.sh
@@ -23,6 +23,16 @@ sudo apt install bluez
 
 
 
+# NodeJS Setup
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+source ~/.bashrc
+
+nvm install 14.17.6
+
+
+
 # Bluefig setup
 
 npm install -g \
