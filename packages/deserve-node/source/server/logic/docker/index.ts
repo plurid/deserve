dedca0a76a97e216b503d2b09598cb1312b24b65
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-09-10 18:52:06 +0300

    deserve-node > dev: server/logic/docker

---
+++ packages/deserve-node/source/server/logic/docker/index.ts
@@ -0,0 +1,17 @@
+// #region imports
+    // #region libraries
+    import Docker from 'dockerode';
+    // #endregion libraries
+// #endregion imports
+
+
+
+// #region module
+const docker = new Docker();
+// #endregion module
+
+
+
+// #region exports
+export default docker;
+// #endregion exports
