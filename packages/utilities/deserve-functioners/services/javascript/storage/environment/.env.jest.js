520dd6535b121b1850f9b0e77342a484fd21f7ae
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-09-11 17:29:19 +0300

    utilities/deserve-functioners/services/javascript/storage > setup: configurations, environment

---
+++ packages/utilities/deserve-functioners/services/javascript/storage/environment/.env.jest.js
@@ -0,0 +1,3 @@
+process.env.DESERVE_DATABASE_ENDPOINT = 'http://localhost:3366/deserve';
+process.env.DESERVE_STORAGE_ENDPOINT = 'http://localhost:3366/';
+process.env.DESERVE_STORAGE_TOKEN = '__test__';
