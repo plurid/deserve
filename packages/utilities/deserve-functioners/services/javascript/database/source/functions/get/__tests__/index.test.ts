671386fc67a99ed97e17ee4f56029fd3c3ab5d6f
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-09-11 10:50:37 +0300

    utilities/deserve-functioners/services/javascript/database > test: functions/get

---
+++ packages/utilities/deserve-functioners/services/javascript/database/source/functions/get/__tests__/index.test.ts
@@ -0,0 +1,18 @@
+// #region imports
+    // #region external
+    import get from '../index';
+    // #endregion external
+// #endregion imports
+
+
+
+// #region module
+describe('get', () => {
+    it(`works`, async () => {
+        const id = '';
+
+        const data = await get(id);
+
+    });
+});
+// #endregion module
