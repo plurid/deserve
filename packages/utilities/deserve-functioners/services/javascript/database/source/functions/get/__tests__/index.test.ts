01602900466b62152086edac1c6fe353b17485fc
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-09-11 15:17:42 +0300

    utilities/deserve-functioners/services/javascript/database > test: functions/get

--- packages/utilities/deserve-functioners/services/javascript/database/source/functions/get/__tests__/index.test.ts
+++ packages/utilities/deserve-functioners/services/javascript/database/source/functions/get/__tests__/index.test.ts
@@ -8,11 +8,15 @@
 
 // #region module
 describe('get', () => {
+    process.env.DATABASE_ENDPOINT = 'http://localhost:3366/graphql';
+    process.env.DATABASE_TOKEN = '__test__';
+
     it(`works`, async () => {
-        const id = '';
+        const id = '1';
 
         const data = await get(id);
 
+        expect(data).toEqual(1);
     });
 });
 // #endregion module
