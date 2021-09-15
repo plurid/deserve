47e4915c34c9fa1dfbd7de84cb6ef321cf332441
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-09-15 14:57:00 +0300

    utilities/deserve-clients/javascript > dev: logic/blobs/store

--- packages/utilities/deserve-clients/javascript/source/logic/blobs/store/index.ts
+++ packages/utilities/deserve-clients/javascript/source/logic/blobs/store/index.ts
@@ -1,4 +1,10 @@
 // #region imports
+    // #region libraries
+    import fetch from 'cross-fetch';
+    import FormData from 'form-data';
+    // #endregion libraries
+
+
     // #region external
     import {
         GraphqlClient,
@@ -23,7 +29,24 @@ const store = (
             };
         }
 
-        const storedID = await new Promise((resolve, reject) => {
+        const storedID = await new Promise(async (resolve, reject) => {
+            const form = new FormData();
+            form.append('file', stream);
+
+            const response = await fetch('uri', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/octet-stream',
+                    ...form.getHeaders(),
+                },
+                body: form.getBuffer(),
+            });
+
+            if (response.status === 200) {
+                resolve(true);
+            } else {
+                reject();
+            }
         });
 
         if (!storedID) {
