d4ae290418e30ff9db213801d0509d97dd7fa23e
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-09-17 12:40:14 +0300

    utilities/deserve-functioners/services/javascript/storage > dev: data/constants

--- packages/utilities/deserve-functioners/services/javascript/storage/source/data/constants/index.ts
+++ packages/utilities/deserve-functioners/services/javascript/storage/source/data/constants/index.ts
@@ -1,11 +1,18 @@
+// #region imports
+    // #region libraries
+    import {
+        url,
+    } from '@plurid/plurid-functions';
+    // #endregion libraries
+// #endregion imports
+
+
+
 // #region module
-export const DESERVE_ENDPOINT = process.env.DESERVE_ENDPOINT || ''
-export const DESERVE_ENDPOINT_RESOLVED = DESERVE_ENDPOINT.endsWith('/')
-    ? DESERVE_ENDPOINT.slice(0, DESERVE_ENDPOINT.length - 1)
-    : DESERVE_ENDPOINT;
+export const DESERVE_ENDPOINT = url.removeTrailingSlash(process.env.DESERVE_ENDPOINT || '');
 
-export const DATABASE_ENDPOINT = DESERVE_ENDPOINT_RESOLVED + '/deserve';
+export const DATABASE_ENDPOINT = DESERVE_ENDPOINT + '/deserve';
 
-export const STORAGE_ENDPOINT = DESERVE_ENDPOINT_RESOLVED;
+export const STORAGE_ENDPOINT = DESERVE_ENDPOINT;
 export const STORAGE_TOKEN = process.env.DESERVE_STORAGE_TOKEN;
 // #endregion module
