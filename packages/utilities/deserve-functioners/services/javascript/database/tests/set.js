f7ac012d9a7033c1ab1d4c4878b8e66c28806cc0
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-09-17 15:29:22 +0300

    utilities/deserve-functioners/services/javascript/database > test: files

---
+++ packages/utilities/deserve-functioners/services/javascript/database/tests/set.js
@@ -0,0 +1,31 @@
+const curl = require('./utilities/curl').default;
+const runner = require('./utilities/runner').default;
+
+const database = require('../distribution').default;
+
+
+
+const prepare = async () => {
+}
+
+const run = async (
+    preparation,
+) => {
+    process.env.DESERVE_ENDPOINT = 'http://localhost:3366';
+    process.env.DESERVE_DATABASE_TOKEN = '__test__';
+
+    const data = await database.set('id', 'data');
+}
+
+const postpare = async (
+    preparation,
+    result,
+) => {
+}
+
+
+runner(
+    prepare,
+    run,
+    postpare,
+);
