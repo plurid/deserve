9b69dabca9e0c7ba6bfa1f572f7769546beda43c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-09-17 11:42:52 +0300

    utilities/deserve-functioners/services/javascript/storage > dev: data/constants

--- packages/utilities/deserve-functioners/services/javascript/storage/source/data/constants/index.ts
+++ packages/utilities/deserve-functioners/services/javascript/storage/source/data/constants/index.ts
@@ -1,5 +1,11 @@
 // #region module
-export const DATABASE_ENDPOINT = process.env.DESERVE_DATABASE_ENDPOINT;
-export const STORAGE_ENDPOINT = process.env.DESERVE_STORAGE_ENDPOINT;
+export const DESERVE_ENDPOINT = process.env.DESERVE_ENDPOINT || ''
+export const DESERVE_ENDPOINT_RESOLVED = DESERVE_ENDPOINT.endsWith('/')
+    ? DESERVE_ENDPOINT.slice(0, DESERVE_ENDPOINT.length - 1)
+    : DESERVE_ENDPOINT;
+
+export const DATABASE_ENDPOINT = DESERVE_ENDPOINT_RESOLVED + '/deserve';
+
+export const STORAGE_ENDPOINT = DESERVE_ENDPOINT_RESOLVED;
 export const STORAGE_TOKEN = process.env.DESERVE_STORAGE_TOKEN;
 // #endregion module
