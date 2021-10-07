fb21a8386cc63aedcf20e520b0af6a0a604e8371
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-10-07 19:06:42 +0300

    deserve-node > dev: server/api/models/store/blobs/download | range

--- packages/deserve-node/source/server/api/models/store/blobs/download/index.ts
+++ packages/deserve-node/source/server/api/models/store/blobs/download/index.ts
@@ -2,6 +2,8 @@
     // #region libraries
     import express from 'express';
 
+    import RangeParser from 'range-parser';
+
     import delog from '@plurid/delog';
     // #endregion libraries
 
@@ -91,10 +93,83 @@ const download = async (
         const {
             ownerID,
             blobSHA,
+            size,
         } = blobData;
 
         const blobLocation = ownerID + '/' + blobSHA;
 
+
+
+        /**
+         * Handle Range Request
+         */
+        const rangeRequest = request.range(size);
+        if (
+            rangeRequest
+            && (rangeRequest as RangeParser.Ranges).type === 'bytes'
+        ) {
+            const MAXIMUM_PAYLOAD = 50_000_000;
+
+            const PAYLOAD_TOO_LARGE = 413;
+            const RANGE_NOT_SATISFIABLE = 416;
+
+            const range = (rangeRequest as RangeParser.Ranges)[0];
+            if (range) {
+                const {
+                    start,
+                    end,
+                } = range;
+
+                if (start >= size || end >= size) {
+                    response.status(RANGE_NOT_SATISFIABLE);
+                    response.setHeader('Content-Range', `bytes */${size}`);
+                    response.end();
+
+                    return;
+                }
+
+
+                const chunkSize = end - start;
+
+                if (chunkSize > MAXIMUM_PAYLOAD) {
+                    response.status(PAYLOAD_TOO_LARGE);
+                    response.setHeader('Content-Range', `bytes */${size}`);
+                    response.end();
+
+                    return;
+                }
+
+
+                const partial = await storage.object.readPartial(
+                    DESERVE_BLOBS,
+                    blobLocation,
+                    start,
+                    end,
+                );
+                if (!partial) {
+                    response.status(404).end();
+                    return;
+                }
+
+                if (end !== size) {
+                    response.status(206);
+                } else {
+                    response.status(200);
+                }
+
+                response.setHeader('Content-Range', `bytes ${start}-${end}/${size}`);
+                response.setHeader('Content-Length', chunkSize);
+
+                response.end(partial.data);
+                return;
+            }
+        }
+
+
+
+        /**
+         * Handle Stream Request
+         */
         const outStream = await storage.object.readAsStream(
             DESERVE_BLOBS,
             blobLocation,
