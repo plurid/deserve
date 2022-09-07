e94b3041356a3f575c8a60f67ed5c7d6ee21bec2
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-09-07 15:42:41 +0300

    deserve-node > dev: server/services/storage

--- packages/deserve-node/source/server/services/storage/index.ts
+++ packages/deserve-node/source/server/services/storage/index.ts
@@ -9,5 +9,12 @@
 // #region exports
 export * from '~server/data/constants/storage';
 
+storage.client.generate({
+    endPoint: process.env.DESERVE_NODE_MINIO_END_POINT || '',
+    port: parseInt(process.env.DESERVE_NODE_MINIO_PORT || ''),
+    accessKey: process.env.DESERVE_NODE_MINIO_ACCESS_KEY || '',
+    secretKey: process.env.DESERVE_NODE_MINIO_SECRET_KEY || '',
+});
+
 export default storage;
 // #endregion exports
